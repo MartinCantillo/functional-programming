8867bba9beae3702f2edc7fcda179988d3d2698b
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-12-25 21:04:33 -0500

    adding Predicate funtional interface

--- funtionalProgramming/src/App.java
+++ funtionalProgramming/src/App.java
@@ -1,8 +1,19 @@
  import interfaces.FunctionR2R;
+import java.util.function.Predicate;
 public class App {
     public static void main(String[] args)  {
        
         FunctionR2R sqr=x->x*x;
         System.out.println(sqr.valueAt(4));
+
+
+        /** funtional interfaces  */
+
+        /**Predicate interface returns a boolean value,
+         * Predicate has a only abstract method named test
+        */
+        Predicate<Integer> isEven=x->x%2==0;
+
+        System.out.println(isEven.test(5));
     }
 }
