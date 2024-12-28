bc5d15f2971ee92a8c435c858bc59c7d0c222d9c
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-12-28 09:32:01 -0500

    Adding referecesMethod class

---
+++ funtionalProgramming/src/ReferencesMethod.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.util.function.Consumer;
+import models.Person;
+
+public class ReferencesMethod {
+public static void main(String[] args) {
+
+
+    /**static method references (class::Staticmethod) */
+
+    List<String> names= Arrays.asList("Martin","Camilo","Andres");
+
+   /**Person=class , printNames= method */
+    Consumer<String>printer=Person::printNames;
+    
+    names.forEach(printer);
+    
+}
+}
