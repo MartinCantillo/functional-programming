61508fa77471e8f2a562b1b34a1c4e00ce393f65
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-12-28 09:10:33 -0500

    Adding Consumer interface

--- funtionalProgramming/src/App.java
+++ funtionalProgramming/src/App.java
@@ -1,4 +1,7 @@
  import interfaces.FunctionR2R;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 public class App {
@@ -23,5 +26,20 @@ public class App {
           */
           Function<Integer,Double> square=x->(double)x*x;//I want to return a value of type double and I a  input int value 
           System.out.println(square.apply(4));
+
+        // The Consumer interface in Java represents an operation that takes a single input argument 
+        // and does not return a result. It is often used for performing side effects, 
+        // such as logging, printing, or modifying external state.
+          Consumer<String> printString = s -> System.out.println(s);
+
+          printString.accept("Hello World");
+
+
+          //Another example de consumer , i will use it for printing the list
+
+          List<String> names= Arrays.asList("Martin","Camilo","Andres");
+        
+          names.forEach(printString);
+
     }
 }
